a090edcd8c5509ddf43c03aec236f81c8522ef3e
Hyagobsantos <hyagobezerra64@gmail.com>
2021-10-30 00:37:16 -0300

    fix dotenv

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      forbidNonWhitelisted: true
-    })
-  )
-  await app.listen(3000);
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(process.env.Port || 3000);
 }
 bootstrap();
